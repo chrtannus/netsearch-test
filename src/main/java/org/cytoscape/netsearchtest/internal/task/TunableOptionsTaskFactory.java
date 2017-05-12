ef89248b8b4c45a57ffb98f01dbbbd8993aae2af
Christian Lopes <chrtannus@gmail.com>
2017-05-12 13:42:25 -0400

    Adds additional Tunables to options test.

--- src/main/java/org/cytoscape/netsearchtest/internal/task/TunableOptionsTaskFactory.java
+++ src/main/java/org/cytoscape/netsearchtest/internal/task/TunableOptionsTaskFactory.java
@@ -1,11 +1,19 @@
 package org.cytoscape.netsearchtest.internal.task;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
 import org.cytoscape.work.Tunable;
+import org.cytoscape.work.swing.util.UserAction;
+import org.cytoscape.work.util.BoundedFloat;
+import org.cytoscape.work.util.ListMultipleSelection;
+import org.cytoscape.work.util.ListSingleSelection;
 
 /**
  * Testing the use of Tunables for the Network Search's extra options.
  */
-public class TunableOptionsTaskFactory extends AbstractNetSearchTestTaskFactory {
+public class TunableOptionsTaskFactory extends AbstractNetSearchTestTaskFactory implements ActionListener {
 
 	@Tunable(description = "Test String:")
 	public String testString;
@@ -19,11 +27,31 @@ public class TunableOptionsTaskFactory extends AbstractNetSearchTestTaskFactory 
 	@Tunable(description = "Test Boolean:")
 	public boolean testBoolean = true;
 	
+	@Tunable(description = "Test Slider:", params = "slider=true")
+	public BoundedFloat testBoundedFloat = new BoundedFloat(0.0f, 5.0f, 10.0f, false, false);
+	
+	@Tunable(description = "Test Combo:")
+	public ListSingleSelection<String> testList1 = new ListSingleSelection<>("value 1", "value 2", "value 3");
+	
+	@Tunable(description = "Test List:")
+	public ListMultipleSelection<String> testList2 = new ListMultipleSelection<>("value A", "value B", "value C");
+	
+	@Tunable(description="Test File:")
+	public File file;
+	
+	@Tunable(description="Test UserAction")
+	public UserAction testUserAction = new UserAction(this);
+	
 	public TunableOptionsTaskFactory() {
 		super(
 				"netsearchtest.test-a",
 				"A. Tunable Options",
 				"The seearch options are auto-generated from Tunables"
 		);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Test UserAction clicked!");
 	}
 }
